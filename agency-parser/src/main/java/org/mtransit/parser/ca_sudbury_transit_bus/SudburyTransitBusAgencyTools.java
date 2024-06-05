f00317b908e32e6bcadacdb6f00442db372090ff
Mathieu Méa <mathieu.mea@gmail.com>
2024-06-04 20:27:32 -0400

    Migrate to new flatten git repo

---
+++ agency-parser/src/main/java/org/mtransit/parser/ca_sudbury_transit_bus/SudburyTransitBusAgencyTools.java
@@ -0,0 +1,138 @@
+package org.mtransit.parser.ca_sudbury_transit_bus;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.mtransit.commons.CleanUtils;
+import org.mtransit.commons.Letters;
+import org.mtransit.commons.provider.GreaterSudburyProviderCommons;
+import org.mtransit.parser.ColorUtils;
+import org.mtransit.parser.DefaultAgencyTools;
+import org.mtransit.parser.gtfs.data.GStop;
+import org.mtransit.parser.mt.data.MAgency;
+import org.mtransit.parser.mt.data.MRouteSNToIDConverter;
+
+import java.util.List;
+import java.util.Locale;
+
+// https://opendata.greatersudbury.ca/
+// https://opendata.greatersudbury.ca/search?collection=Dataset&q=Transportation
+// https://opendata.greatersudbury.ca/documents/Sudbury::transit-schedule-data-gtfs/about
+public class SudburyTransitBusAgencyTools extends DefaultAgencyTools {
+
+	public static void main(@NotNull String[] args) {
+		new SudburyTransitBusAgencyTools().start(args);
+	}
+
+	@Nullable
+	@Override
+	public List<Locale> getSupportedLanguages() {
+		return LANG_EN_FR;
+	}
+
+	@NotNull
+	@Override
+	public String getAgencyName() {
+		return "Sudbury Transit";
+	}
+
+	@Override
+	public boolean defaultExcludeEnabled() {
+		return true;
+	}
+
+	@NotNull
+	@Override
+	public Integer getAgencyRouteType() {
+		return MAgency.ROUTE_TYPE_BUS;
+	}
+
+	@Override
+	public boolean defaultRouteIdEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean useRouteShortNameForRouteId() {
+		return true;
+	}
+
+	@Override
+	public boolean defaultRouteLongNameEnabled() {
+		return true;
+	}
+
+	@NotNull
+	@Override
+	public String cleanRouteLongName(@NotNull String routeLongName) {
+		routeLongName = CleanUtils.cleanSlashes(routeLongName);
+		routeLongName = CleanUtils.cleanStreetTypes(routeLongName);
+		routeLongName = CleanUtils.cleanNumbers(routeLongName);
+		return CleanUtils.cleanLabel(routeLongName);
+	}
+
+	@Nullable
+	@Override
+	public String fixColor(@Nullable String color) {
+		if (ColorUtils.BLACK.equalsIgnoreCase(color)) {
+			return null;
+		}
+		return super.fixColor(color);
+	}
+
+	@Override
+	public boolean defaultAgencyColorEnabled() {
+		return true;
+	}
+
+	private static final String AGENCY_COLOR_GREEN = "005941"; // GREEN (from web site CSS)
+
+	private static final String AGENCY_COLOR = AGENCY_COLOR_GREEN;
+
+	@NotNull
+	@Override
+	public String getAgencyColor() {
+		return AGENCY_COLOR;
+	}
+
+	@Override
+	public boolean directionSplitterEnabled(long routeId) {
+		//noinspection RedundantIfStatement
+		if (routeId == 1L + MRouteSNToIDConverter.endsWith(Letters.S)) { // 1S
+			return false; // 2024-02-16: impossible to split
+		}
+		return true;
+	}
+
+	@Override
+	public boolean directionFinderEnabled() {
+		return true;
+	}
+
+	@NotNull
+	@Override
+	public String cleanTripHeadsign(@NotNull String tripHeadsign) {
+		return GreaterSudburyProviderCommons.cleanTripHeadSign(tripHeadsign);
+	}
+
+	@NotNull
+	@Override
+	public String cleanStopName(@NotNull String gStopName) {
+		gStopName = CleanUtils.cleanNumbers(gStopName);
+		gStopName = CleanUtils.fixMcXCase(gStopName);
+		// gStopName = CleanUtils.cleanBounds(gStopName); // TODO maybe later w/ conditional text shortening relative to target length
+		gStopName = CleanUtils.cleanStreetTypes(gStopName);
+		return CleanUtils.cleanLabel(gStopName);
+	}
+
+	@NotNull
+	@Override
+	public String getStopCode(@NotNull GStop gStop) {
+		//noinspection deprecation
+		return gStop.getStopId(); // use stop ID as stop code, used by real-time API, do not change
+	}
+
+	@Override
+	public int getStopId(@NotNull GStop gStop) {
+		return super.getStopId(gStop); // used by real-time API, do not change
+	}
+}
